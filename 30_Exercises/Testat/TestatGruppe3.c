918d76093d82e6e53209e4a27a0617c351923756
gutandrHSLU <andre.gut@stud.hslu.ch>
2020-10-28 10:31:15 +0100

    myDatapoints to .txt added

--- 30_Exercises/Testat/TestatGruppe3.c
+++ 30_Exercises/Testat/TestatGruppe3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 //Structure of a DataPoint
 struct DataPoint{
@@ -10,10 +11,12 @@ struct DataPoint{
   char alarmState;
 }__attribute__((packed));
 
-//Initialisierung der Funktionen
+//Initialisierung der allgemeinen Funktionen
 void PrintDataPoints(struct DataPoint dataPoints[], int numberOfPoints);
 void PrintTime(long long timeStamp);
 void PrintTitle(int numberOfTask);
+void WriteDataTotxt(struct DataPoint dataPoints[], int numberOfPoints, int* pointer);
+int* IntToBinaryArray(int num, int* bitsArray);
 
 //Funktion für Aufg. 1
 
@@ -31,17 +34,21 @@ int ArraySum(int points[], int arrayLength);
 
 
 int main(int argc, char *argv[]){
-	FILE *fid = fopen("pressureSpike.bin", "rb"); //Öffnet das File und initialisiert einen Pointer:
+	FILE *fid = fopen("pressureSpike.bin", "rb"); //Öffnet das bressureSpike.bin File und initialisiert einen Pointer:
+  FILE *txtFileP; //Initialisiert den Pointer zum leeren .txt file
+  txtFileP = fopen("pressureSpike.txt", "w"); //Öffnet das neue .txt file
 
   //Sucht das Ende des Files und berechnet die Anzahl Datenpunkte:
   int numberOfPoints;
   fseek (fid , 0 , SEEK_END);
   numberOfPoints = ftell (fid) / sizeof(struct DataPoint);
-  rewind (fid); //Setzt den Pointer zurück
+  rewind (fid); //Setzt den Pointer zurück (das glaube ich zumindest...)
 
   struct DataPoint myDataPoints[numberOfPoints];   //Array mit DataPoints wird generiert
-  fread(myDataPoints, sizeof(struct DataPoint), numberOfPoints, fid); //Daten werden in das Array mit den DataPoints "abgefüllt":
+  fread(myDataPoints, sizeof(struct DataPoint), numberOfPoints, fid); //Daten werden in das Array "myDataPoints" abgefüllt:
 
+  WriteDataTotxt(myDataPoints, numberOfPoints, txtFileP); //Daten werden von "myDataPoints" in das .txt file geschriben
+  fclose(txtFileP); //.txt file wird geschlossen
 
   //==================== Aufgabe 1 ====================
   PrintTitle(1);
@@ -59,15 +66,12 @@ int main(int argc, char *argv[]){
   //==================== Aufgabe 5 ====================
   PrintTitle(5);
 
-  // PrintTime(myDataPoints[0].timeStamp);
-  // PrintTime(myDataPoints[1].timeStamp);
 
-
-  //PrintDataPoints(myDataPoints, numberOfPoints);
   return 0;
 }
 
 //Implementierung der Funktionen
+
 
 //==============================================================
 //==================== Funktion für Aufg. 1 ====================
@@ -153,7 +157,6 @@ void PrintDataPoints(struct DataPoint dataPoints[], int numberOfPoints){
   return;
 }
 
-//
 void PrintTime(long long timeStamp){
   time_t rawtime = timeStamp / 1000;
   printf("%s", ctime(&rawtime));
@@ -172,4 +175,27 @@ void PrintTitle(int numberOfTask){
   }
   printf("\n");
   return;
+}
+
+void WriteDataTotxt(struct DataPoint dataPoints[], int numberOfPoints, int* pointer){
+  for(int i = 0; i < numberOfPoints-1; i++){
+    time_t rawtime = dataPoints[i].timeStamp / 1000;
+    int bits[4];
+    int* bitsP = IntToBinaryArray(dataPoints[i].systemState, bits);
+    fprintf (pointer, "%.24s; %d; %d; %d; %d; %d; %d; \n", ctime(&rawtime), dataPoints[i].pressure, bitsP[3], bitsP[2], bitsP[1], bitsP[0], dataPoints[i].alarmState);
+  }
+  return;
+}
+
+int* IntToBinaryArray(int num, int* bitsArray){
+	int counter = 0;
+	while(num){
+		if(num&1)
+			bitsArray[counter] = 1;
+		else
+			bitsArray[counter] = 0;
+		num>>=1;
+		counter++;
+	}
+	return bitsArray;
 }
